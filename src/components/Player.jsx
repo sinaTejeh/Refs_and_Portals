0bc46961c34ccd47967f90028eeb2c4db84e9983
sinaTejeh <amrjv1378@gmail.com>
2026-02-20 13:08:26 +0330

    using useRef for getting player name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,19 +1,17 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 export default function Player() {
-  const [enterdPlayerName, setEnterdPlayerName] = useState('');
-  const [submitted, setSubmitted] = useState(false);
-  const handleChange = function (e) {
-    // setSubmitted(false);
-    setEnterdPlayerName(e.target.value);
-  };
+  const playerName = useRef();
+  const [enterdPlayerName, setEnterdPlayerName] = useState(null);
+
   const handleClick = function () {
-    setSubmitted(true);
+    setEnterdPlayerName(playerName.current.value);
+    playerName.current.value = '';
   };
   return (
     <section id="player">
-      <h2>Welcome {submitted ? enterdPlayerName : 'unknown entity'}</h2>
+      <h2>Welcome {enterdPlayerName ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" onChange={handleChange} value={enterdPlayerName} />
+        <input ref={playerName} type="text" />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
